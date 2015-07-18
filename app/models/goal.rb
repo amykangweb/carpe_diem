7886acdb63b62e4267f79f7efe988d9005990638
amykangweb <selene6023@gmail.com>
2015-07-18 16:17:57 -0700

    Add social sharing gem

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -9,6 +9,7 @@ class Goal < ActiveRecord::Base
     self.private == true
   end
 
+# All goals tagged with name arg
   def self.tagged_with(name)
     Tag.find_by_name!(name).goals
   end
@@ -17,6 +18,7 @@ class Goal < ActiveRecord::Base
     tags.map(&:name).join(", ")
   end
 
+# tag setter in goal form
   def tag_list=(names)
     self.tags = names.split(",").map do |n|
       Tag.where(name: n.strip).first_or_create!
