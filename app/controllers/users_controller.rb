5c60f13556304ebef383c09e059da80ecf526661
amykangweb <selene6023@gmail.com>
2015-07-26 00:28:06 -0700

    Paginate goals on user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @goals = @user.goals.paginate(page: params[:page], per_page: 20)
   end
 
   private
