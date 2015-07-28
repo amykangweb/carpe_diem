06a7f2b43d0f94c4517d6623c5cc23448ae44510
amykangweb <selene6023@gmail.com>
2015-07-28 04:08:37 -0700

    Update smtp settings to gmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,12 +6,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :port           => 587,
+    :address        => 'smtp.gmail.com',
+    :user_name      => ENV['email'],
+    :password       => ENV['password'],
     :domain         => 'powerful-badlands-4671.heroku.com',
-    :authentication => :plain,
+    :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
   # Code is not reloaded between requests.
