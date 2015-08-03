390efa837561a173dd3c31231348ec0c66fab193
amykangweb <selene6023@gmail.com>
2015-08-03 00:21:40 -0700

    Add bootstrap gem, navbar, and jumbotron

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'figaro'
 
 gem 'gravtastic'
 
+gem 'bootstrap-sass', '~> 3.3.5'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require social-share-button
 //= require_tree .
